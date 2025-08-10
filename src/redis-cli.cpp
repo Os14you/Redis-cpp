2df303fa1ce5c6890c53ce4f895b58e4bd05d064
Osama <os14you.manage@gmail.com>
2025-08-10 16:44:24 +0300

    feat: edit redis-cli to test the new commands

--- src/redis-cli.cpp
+++ src/redis-cli.cpp
@@ -4,13 +4,39 @@ int main() {
     try {
         Client client("127.0.0.1", 6379); // Connect to the Redis port
 
-        std::vector<std::string> requests = { "PING", "GET mykey", "EXIT", "PING" };
+        std::vector<std::vector<std::string>> requests = {
+            {"SET", "OSAMA", "is the best"},
+            {"GET", "OSAMA"},
+            {"DEL", "OSAMA"},
+            {"GET", "OSAMA"},
+            {"PING"},
+            {"ECHO", "Hello, Redis!"}
+        };
 
         for (const auto& req : requests) {
+            std::cout << "Sending: ";
+            for(const auto& s : req) { std::cout << s << " "; }
+            std::cout << std::endl;
+
+            // The API is now much cleaner!
             client.send(req);
-            std::cout << "Sent: " << req << std::endl;
+
             std::vector<uint8_t> res = client.recv();
-            std::cout << "Received: " << std::string(res.begin(), res.end()) << std::endl;
+
+            // The response from the server is also length-prefixed with a status
+            if (res.size() < 8) {
+                std::cerr << "Invalid response received" << std::endl;
+                continue;
+            }
+
+            uint32_t status;
+            memcpy(&status, res.data() + 4, 4);
+
+            std::cout << "Received Status: " << status;
+            if (res.size() > 8)
+                 std::cout << ", Data: " << std::string(res.begin() + 8, res.end());
+            
+            std::cout << std::endl << "---" << std::endl;
         }
 
     } catch (const std::exception& e) {
