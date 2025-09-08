c8571ac425206e0aad9dd6c3d53a27bd8ce23c94
Osama <os14you.manage@gmail.com>
2025-09-08 18:16:31 +0300

    refactor: edit the redis-client send only one cmd

--- src/redis-cli.cpp
+++ src/redis-cli.cpp
@@ -1,50 +1,33 @@
 #include <Client.hpp>
-#include <vector>
-#include <string>
-#include <iostream>
+#include <Deserialization.hpp>
 #include <cstring>
 
-int main() {
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        std::cerr << "Usage: ./redis-cli <command> [args...]" << std::endl;
+        return 1;
+    }
+
     try {
         Client client("127.0.0.1", 6379); // Connect to the Redis port
 
-        std::vector<std::vector<std::string>> requests = {
-            {"SET", "OSAMA", "best"},
-            {"GET", "OSAMA"},
-            {"DEL", "OSAMA"},
-            {"GET", "OSAMA"},
-            {"PING"},
-            {"ECHO", "Hello, Redis!"} // Note: ECHO is not implemented yet
-        };
-
-        for (const auto& req : requests) {
-            std::cout << "Sending: ";
-            for(const auto& s : req) { std::cout << s << " "; }
-            std::cout << std::endl;
-
-            client.send(req);
-
-            std::vector<uint8_t> res = client.recv();
-
-            // The response payload must have at least 4 bytes for the status code.
-            if (res.size() < 4) {
-                std::cerr << "Invalid response received" << std::endl;
-                continue;
-            }
-
-            uint32_t status;
-            // The status code is at the BEGINNING of the response buffer (offset 0).
-            memcpy(&status, res.data(), 4);
-
-            std::cout << "Received Status: " << status;
-            // The data (if any) starts right AFTER the 4-byte status code.
-            if (res.size() > 4) {
-                 std::cout << ", Data: " << std::string(res.begin() + 4, res.end());
-            }
-
-            std::cout << std::endl << "---" << std::endl;
+        std::vector<std::string> request_cmd;
+        for (int i = 1; i < argc; ++i) {
+            request_cmd.push_back(argv[i]);
         }
 
+        std::cout << "> ";
+        for (const auto &s: request_cmd) { std::cout << s << " "; }
+        std::cout << std::endl;
+
+        client.send(request_cmd);
+        Buffer res = client.recv();
+
+        if(res.empty()) {
+            std::cerr << "Received empty response from server." << std::endl;
+        } else {
+            printResponse(res, 0, 0);
+        }
     } catch (const std::exception& e) {
         std::cerr << "Fatal error: " << e.what() << std::endl;
         return 1;
